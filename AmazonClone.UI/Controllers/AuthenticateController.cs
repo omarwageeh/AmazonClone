3cdc94d70ffb6beaa33436e4cc874c4ff056d18f
Omar Wageeh <omar.wageeh.n@gmail.com>
2023-09-27 04:41:49 +0300

    refactored layout to user _LoginPartial

--- AmazonClone.UI/Controllers/AuthenticateController.cs
+++ AmazonClone.UI/Controllers/AuthenticateController.cs
@@ -1,16 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 using AmazonClone.Model;
 
 namespace AmazonClone.UI.Controllers
